b65ff4eca219b4a74296989d5c3d52987d2c9b41
Fizzify <21076@brightwoods.edu.ph>
2022-04-07 09:47:10 +0800

    Added source.

---
+++ src/components/Quotes.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let quote;
+</script>
+
+<h3>Dune Quotes</h3>
+<h1>"{quote}"</h1>
+
+<style>
+  h1 {
+    font-size: 3rem;
+  }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,46 @@
+<script context="module">
+  export async function load({ fetch }) {
+    const res = await fetch(`https://the-dune-api.herokuapp.com/quotes/1`);
+    const data = await res.json();
+    console.log(data);
+    if (res.ok) {
+      return {
+	props: { quote: data[0].quote }
+      };
+    }
+  }
+</script>
+
+<script>
+  import Quotes from '../components/Quotes.svelte';
+  export let quote;
+</script>
+
+<body>
+  <section>
+    <Quotes {quote}/>
+  </section>
+</body>
+
+<style>
+
+  *,
+  *::after,
+  *::before {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    background-color: #181818;
+  }
+
+  body {
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    background-color: #181818;
+    color: white;
+    font-family: Arial;
+  }
+</style>
